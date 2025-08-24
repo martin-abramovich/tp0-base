3ca9a2b31fd3477a72cb2240a69c64d5326f60dc
Martín Abramovich <abramovich.martin@gmail.com>
2025-08-24 19:41:27 -0300

    Corrección validar-echo-server

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-RESULT=$(echo "hola" | docker run --rm --network testing_net busybox nc server 12345)
+RESULT=$(echo "hola" | docker run --rm --network tp0_testing_net busybox nc server 12345)
 
 if [ "$RESULT" = "hola" ]; then
     echo "action: test_echo_server | result: success"
 else
-    echo "action: test_echo_server | result: failed"
+    echo "action: test_echo_server | result: fail"
 fi
-
